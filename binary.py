b9797c917cb9f7df67ef2c230f32a6ec6d2807ad
Puujee42 <kayanokouji22@gmail.com>
2025-10-31 13:20:41 +0800

    binary adder and divider added

--- binary.py
+++ binary.py
@@ -11,47 +11,81 @@ def  binary_adder(bin1,bin2):
     len1 = len(bin1)
     len2 = len(bin2)
     collect = []
-    if len1 >= len2:
-        collect = [0] * (len1 + 1)
+    if len1 > len2:
         for i in range(len1 - len2):
             bin2.insert(0,0)
-        bin1.reverse()
-        bin2.reverse()
-        for i in range(len1-1,0):
+        for i in range(0,len(bin2)):
+            collect.append(0)
+        for i in range(len(bin2)-1,-1,-1):
             if bin1[i] == 1 and bin2[i] == 1:
-                if i != len1 - 1:
-                    if collect[i] == 1:
-                        pass
-                    else:
-                        collect[i + 1] = 1
-                        collect[i] = 0
-                elif bin1[i] == 1 and bin2[i] == 0:
-                    if collect[i] == 1:
-                        pass
-                    else:
-                        collect[i] = 1
-                elif bin1[i] == 0 and bin2[i] == 1:
-                    if collect[i] == 1:
-                        pass
-                    else:
-                        collect[i] = 1
-                elif bin1[i] == 0 and bin2[i] == 0:
-                    if collect[i] == 1:
-                        pass
-                    else:
-                        collect[i] = 0
+                if collect[i] == 1  and i < len(bin2)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin2)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
                 else:
-                    if collect[i] == 1:
-                        pass
-                    else:
-                        collect[i] = 0
-                        collect[i + 1] = 1
-    else:
+                    collect[i] = 1
+            elif bin1[i] == 0 and bin2[i] == 1:
+                if collect[i] == 1  and i < len(bin2)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin2)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
+                else:
+                    collect[i] = 1
+            elif bin1[i] == 1 and bin2[i] == 0:
+                if collect[i] == 1  and i < len(bin2)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin2)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
+                else:
+                    collect[i] = 1
+            else:
+                pass
+    elif len2 > len1:
         for i in range(len2 - len1):
             bin1.insert(0,0)
+        print(bin1)
+        for i in range(0,len(bin1)):
+            collect.append(0)
+        print(collect)
+        for i in range(len(bin1)-1,-1,-1):
+            if bin1[i] == 1 and bin2[i] == 1:
+                if collect[i] == 1  and i < len(bin1)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin1)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
+                else:
+                    collect[i] = 1
+            elif bin1[i] == 0 and bin2[i] == 1:
+                if collect[i] == 1  and i < len(bin1)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin1)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
+                else:
+                    collect[i] = 1
+            elif bin1[i] == 1 and bin2[i] == 0:
+                if collect[i] == 1  and i < len(bin1)-1:
+                    collect[i] = 1
+                    collect[i-1] = 1
+                elif collect[i] ==  1 and i == len(bin1)-1:
+                    collect[i] = 0
+                    collect.insert(0,1)
+                else:
+                    collect[i] = 1
+            else:
+                pass
     print(collect)
 def calculate(value1,value2):
     binary1 = binary_divider(value1)
     binary2 = binary_divider(value2)
     binary_adder(binary1,binary2)
-calculate(3,2)
+calculate(100,10000)
